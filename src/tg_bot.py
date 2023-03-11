02b3e44c294f1ed124b76d2eda44f62db97f3257
Orb <admin@lnorb.com>
2023-03-11 16:02:59 +0800

    added start / help menu

--- src/tg_bot.py
+++ src/tg_bot.py
@@ -3,6 +3,8 @@ import tempfile
 import os
 import requests
 from telegram import __version__ as TG_VER, ForceReply, Update
+from telegram.ext import CommandHandler, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.constants import ChatAction
 from telegram.ext import (
     Application,
@@ -14,7 +16,6 @@ from telegram.ext import (
 
 from textwrap import dedent
 
-import sys
 from utils import think, speak, Entry, Speaker, Conversation
 from record import record
 from whisper import whisper
@@ -24,15 +25,54 @@ keys.set_up_keys()
 convos = {}
 
 
+async def intro(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user = update.effective_user
+
+    async def typing():
+        await context.bot.send_chat_action(
+            chat_id=update.effective_chat.id, action=ChatAction.TYPING
+        )
+
+    await typing()
+
+    msg = "Hello and welcome to Chat Jeep Tea. To speak to me, record and send a voice message by doing a long press on the microphone icon at the bottom right of telegram. I will respond to your message. To bring up the help menu at any time, type /help. Please bear in mind I keep a certain (somewhat small) amount of the conversation history as context. This helps us have a more natural conversation. If you want to start a new conversation simply press the 'new conversation' button in the help menu. However this is usually not required as i deal well with context switching. Go ahead, ask me anything."
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text=f"assistant: {msg}"
+    )
+
+    await typing()
+
+    fn = speak(
+        msg,
+        use_google=True,
+        save_only=True,
+    )
+    await context.bot.send_voice(chat_id=update.effective_chat.id, voice=fn)
+
+
+async def new_conversation(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Done")
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.effective_user
-    await update.message.reply_html(
-        rf"Hi {user.mention_html()}!", reply_markup=ForceReply(selective=True)
-    )
+    await update.message.reply_html(rf"Hi {user.mention_html()}!")
+    await update.message.reply_text("Help Menu:", reply_markup=main_menu_keyboard())
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Help!")
+    await update.message.reply_text("Help menu:", reply_markup=main_menu_keyboard())
+
+
+def main_menu_keyboard():
+    keyboard = [
+        [
+            InlineKeyboardButton("New Conversation", callback_data="new_conversation"),
+            InlineKeyboardButton("What is this?", callback_data="intro"),
+        ]
+    ]
+    return InlineKeyboardMarkup(keyboard)
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -94,6 +134,11 @@ def main() -> None:
             f"This example is not compatible with your current PTB version {TG_VER}."
         )
     application = Application.builder().token(os.environ["TOKEN"]).build()
+    # application.add_handler(CallbackQueryHandler(main_menu, pattern="main"))
+    application.add_handler(
+        CallbackQueryHandler(new_conversation, pattern="new_conversation")
+    )
+    application.add_handler(CallbackQueryHandler(intro, pattern="intro"))
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(
