141a371419195958479a609bc4d20bfd4f3c018e
Orb <admin@lnorb.com>
2023-03-14 17:39:15 +0800

    try execpt

--- src/longform.py
+++ src/longform.py
@@ -11,7 +11,7 @@ data_format = json.dumps(
                 "title": "CHAPTER TITLE",
                 "sections": [
                     {
-                        "title": "SECION NAME",
+                        "title": "SECTION NAME",
                         "content": "SECTION CONTENT",
                     }
                 ],
@@ -28,20 +28,25 @@ async def generate_longform(convo, user, context, update):
     prompt = f"Write the outline as a json document for a book entitled: {title}. Please only respond with the json and nothing else. The json must be in the following format: \n\n{data_format}"
     print(prompt)
     convo.add_entry(prompt, Speaker.user)
-    # think(convo)
+    think(convo)
     response = convo.last_entry()
-    prompts = generate_prompts(data)
+    await update.message.reply_text(f"assistant: {convo.last_entry()}")
+    print(response)
+    prompts = generate_prompts(json.loads(response))
     convo.delete_cache()
     for p in prompts:
         convo.add_entry(p, Speaker.user)
-        think(convo)
+        try:
+            think(convo)
+        except:
+            print("error thinking")
         fn = speak(
             text=convo.last_entry(), user_id=user.id, use_google=True, save_only=True
         )
         try:
             await context.bot.send_voice(chat_id=update.effective_chat.id, voice=fn)
         except:
-            pass
+            print("error sending voice")
 
 
 def generate_prompts(data):
@@ -54,98 +59,98 @@ def generate_prompts(data):
     return prompts
 
 
-data = {
-    "title": "The art of living care free",
-    "author": "Unknown",
-    "chapters": [
-        {
-            "title": "Introduction",
-            "sections": [{"title": "", "content": "Importance of living care free"}],
-        },
-        {
-            "title": "Chapter 1: Understanding the concept of living care free",
-            "sections": [
-                {
-                    "title": "Section 1: Defining care free living",
-                    "content": "What it means to live care free",
-                },
-                {
-                    "title": "Section 2: Common misconceptions",
-                    "content": "Dispelling myths about living care free",
-                },
-            ],
-        },
-        {
-            "title": "Chapter 2: Preparing for a care free life",
-            "sections": [
-                {
-                    "title": "Section 1: Embracing change",
-                    "content": "The importance of being open to change",
-                },
-                {
-                    "title": "Section 2: Letting go",
-                    "content": "Tips for letting go of things that hold you back",
-                },
-            ],
-        },
-        {
-            "title": "Chapter 3: Simple habits for a care free life",
-            "sections": [
-                {
-                    "title": "Section 1: Decluttering",
-                    "content": "The benefits of decluttering your life and how to do it",
-                },
-                {
-                    "title": "Section 2: Mindfulness",
-                    "content": "The role of mindfulness in living care free",
-                },
-                {
-                    "title": "Section 3: Prioritizing self-care",
-                    "content": "How taking care of yourself can lead to a care free life",
-                },
-            ],
-        },
-        {
-            "title": "Chapter 4: Overcoming obstacles to a care free life",
-            "sections": [
-                {
-                    "title": "Section 1: Fear",
-                    "content": "How to overcome fear and take risks",
-                },
-                {
-                    "title": "Section 2: Negative self-talk",
-                    "content": "Recognizing and changing negative self-talk",
-                },
-                {
-                    "title": "Section 3: Other people's expectations",
-                    "content": "The danger of living up to other people's expectations and how to break free",
-                },
-            ],
-        },
-        {
-            "title": "Chapter 5: Maintaining a care free life",
-            "sections": [
-                {
-                    "title": "Section 1: Staying motivated",
-                    "content": "Tips for staying motivated to live care free",
-                },
-                {
-                    "title": "Section 2: Incorporating care free habits into daily life",
-                    "content": "How to make care free living a permanent part of your life",
-                },
-            ],
-        },
-        {
-            "title": "Conclusion",
-            "sections": [
-                {
-                    "title": "",
-                    "content": "Final thoughts and encouragement to continue living care free",
-                }
-            ],
-        },
-    ],
-}
+# data = {
+#     "title": "The art of living care free",
+#     "author": "Unknown",
+#     "chapters": [
+#         {
+#             "title": "Introduction",
+#             "sections": [{"title": "", "content": "Importance of living care free"}],
+#         },
+#         {
+#             "title": "Chapter 1: Understanding the concept of living care free",
+#             "sections": [
+#                 {
+#                     "title": "Section 1: Defining care free living",
+#                     "content": "What it means to live care free",
+#                 },
+#                 {
+#                     "title": "Section 2: Common misconceptions",
+#                     "content": "Dispelling myths about living care free",
+#                 },
+#             ],
+#         },
+#         {
+#             "title": "Chapter 2: Preparing for a care free life",
+#             "sections": [
+#                 {
+#                     "title": "Section 1: Embracing change",
+#                     "content": "The importance of being open to change",
+#                 },
+#                 {
+#                     "title": "Section 2: Letting go",
+#                     "content": "Tips for letting go of things that hold you back",
+#                 },
+#             ],
+#         },
+#         {
+#             "title": "Chapter 3: Simple habits for a care free life",
+#             "sections": [
+#                 {
+#                     "title": "Section 1: Decluttering",
+#                     "content": "The benefits of decluttering your life and how to do it",
+#                 },
+#                 {
+#                     "title": "Section 2: Mindfulness",
+#                     "content": "The role of mindfulness in living care free",
+#                 },
+#                 {
+#                     "title": "Section 3: Prioritizing self-care",
+#                     "content": "How taking care of yourself can lead to a care free life",
+#                 },
+#             ],
+#         },
+#         {
+#             "title": "Chapter 4: Overcoming obstacles to a care free life",
+#             "sections": [
+#                 {
+#                     "title": "Section 1: Fear",
+#                     "content": "How to overcome fear and take risks",
+#                 },
+#                 {
+#                     "title": "Section 2: Negative self-talk",
+#                     "content": "Recognizing and changing negative self-talk",
+#                 },
+#                 {
+#                     "title": "Section 3: Other people's expectations",
+#                     "content": "The danger of living up to other people's expectations and how to break free",
+#                 },
+#             ],
+#         },
+#         {
+#             "title": "Chapter 5: Maintaining a care free life",
+#             "sections": [
+#                 {
+#                     "title": "Section 1: Staying motivated",
+#                     "content": "Tips for staying motivated to live care free",
+#                 },
+#                 {
+#                     "title": "Section 2: Incorporating care free habits into daily life",
+#                     "content": "How to make care free living a permanent part of your life",
+#                 },
+#             ],
+#         },
+#         {
+#             "title": "Conclusion",
+#             "sections": [
+#                 {
+#                     "title": "",
+#                     "content": "Final thoughts and encouragement to continue living care free",
+#                 }
+#             ],
+#         },
+#     ],
+# }
 
 
-# print(generate_prompts(data))
+# # print(generate_prompts(data))
