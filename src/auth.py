1f9ae0a3559e89bbf5789eb582a392910ab1d9eb
Orb <admin@lnorb.com>
2023-03-19 07:41:02 +0800

    google.auth.exceptions.DefaultCredentialsError: File key.json was not found. #3

--- src/auth.py
+++ src/auth.py
@@ -1,9 +1,13 @@
+import os
 import json
 import asyncio
 import aiofiles
 
 
-async def load_auth_data(file_path):
+async def load_auth_data(file_path, user_id):
+    if not os.path.exists(file_path):
+        async with aiofiles.open(file_path, "w") as w:
+            await w.write(json.dumps([user_id]))
     async with aiofiles.open(file_path, "r") as f:
         data = await f.read()
     return json.loads(data)
@@ -12,7 +16,7 @@ async def load_auth_data(file_path):
 async def check_is_authorized(user_id):
     file_path = "src/auth.json"
     try:
-        auth_data = await load_auth_data(file_path)
+        auth_data = await load_auth_data(file_path, user_id)
         return user_id in auth_data
     except Exception as e:
         print(f"Error while checking authorization: {e}")
